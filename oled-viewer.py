a7d741108d8d48c07149cb6597a6a3d680954e64
souviksaha97 <souvikssaha@gmail.com>
2020-04-26 16:06:49 +0530

    Resized before thresholding

--- oled-viewer.py
+++ oled-viewer.py
@@ -9,7 +9,7 @@ import time
 WIDTH = 128
 HEIGHT = 64 # Change to 32 depending on your screen resolution
 
-cap = cv2.VideoCapture('vid1.mp4') #Enter the name of your video in here
+cap = cv2.VideoCapture('vid2.webm') #Enter the name of your video in here
 oled_reset = digitalio.DigitalInOut(board.D4)
 
 i2c = board.I2C()
@@ -18,28 +18,32 @@ oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3c, reset=oled_re
 image = Image.new('1', (oled.width, oled.height))
 draw = ImageDraw.Draw(image)
 frameCounter = 0
-frameSkip = 10 #Change to adjust frame rate
-lowerThresh = 240 # Adjust threshold according to video
+frameSkip = 1 #Change to adjust frame rate
+lowerThresh = 0# Adjust threshold according to video
 while(cap.isOpened()):
     ret, frame = cap.read()
+    frameStart = time.time()
     if ret==True:
         if frameCounter%frameSkip == 0:
             # Import image as grayscale
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+##            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            resized = cv2.resize(frame, (oled.width, oled.height))
 
             # Resize to fit OLED screen dimensions
-            resized = cv2.resize(gray, (oled.width, oled.height))
+##            resized = cv2.resize(gray, (oled.width, oled.height))
+            gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 
             # Threshold it to B&W
-            (thresh, bw) = cv2.threshold(resized, lowerThresh, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+            (thresh, bw) = cv2.threshold(gray, lowerThresh, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
             # Clear screen for next frame
             draw.rectangle((0, 0, oled.width, oled.height),
                        outline=0, fill=0)
-
+##            oled.fill(0)
             # Convert to OLED format, and print
             screenframe = Image.fromarray(bw).convert("1")
             oled.image(screenframe)
             oled.show()
-            
+            frameEnd = time.time()
+            print(1/(frameEnd-frameStart))
         frameCounter=frameCounter+1
